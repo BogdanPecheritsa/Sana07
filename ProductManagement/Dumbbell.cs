237520a185a3ec67cd2df6c19505edc9c41ff148
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-30 18:57:30 +0200

    Implemented exception handling for the Dumbbell class

--- ProductManagement/Dumbbell.cs
+++ ProductManagement/Dumbbell.cs
@@ -5,10 +5,47 @@ namespace ProductManagement
 {
     public class Dumbbell : Product
     {
-        public double Weight { get; set; }
-        public string MaterialType { get; set; }
-        public bool Adjustable { get; set; }
-        public string TextureType { get; set; }
+        protected double weight;
+        public double Weight
+        {
+            get => weight;
+            set
+            {
+                if (value >= 0)
+                    weight = value;
+                else throw new ArgumentException("Weight cannot be negative.");
+            }
+        }
+        protected string materialType;
+        public string MaterialType
+        {
+            get => materialType;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException("MaterialType cannot be null.");
+                else
+                    materialType = value;
+            }
+        }
+        protected bool adjustable;
+        public bool Adjustable
+        {
+            get => adjustable;
+            set => adjustable = value;
+        }
+        protected string textureType;
+        public string TextureType
+        {
+            get => textureType;
+            set
+            {
+                if (value == null)
+                    throw new ArgumentException("TextureType cannot be empty or whitespace.");
+                else
+                    textureType = value;
+            }
+        }
 
         public Dumbbell(string name, double price, int count, double weight, string materialType, string textureType, bool adjustable)
             : base(name, price, count)
