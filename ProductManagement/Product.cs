ada22eb6eb586a628a911169cfebfbb733e59ccd
BogdanPecheritsa <ipz231_pbd@student.ztu.edu.ua>
2024-01-30 15:10:17 +0200

    Implemented abstract base class Product, which includes properties Name, Price & Count, constructor and abstract method Display()

--- ProductManagement/Product.cs
+++ ProductManagement/Product.cs
@@ -1,7 +1,17 @@
 ﻿namespace ProductManagement
 {
-    public class Class1
+    public abstract class Product
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+        public abstract string Display();
 
+        public Product(string name, double price, int count)
+        {
+            Name = name;
+            Price = price;
+            Count = count;
+        }
     }
 }
